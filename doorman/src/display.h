d8c34564bbfc5c4406c9dd8ffe63ca0d2c353523
Cyril Stoller <cyril.stoller@gmail.com>
2024-10-03 08:44:27 +0200

    Working separate display class

--- doorman/src/display.h
+++ doorman/src/display.h
@@ -17,34 +17,37 @@ const uint8_t bitmap_wifi_on[] = {0x00, 0x00, 0x00, 0x00, 0x07, 0x00, 0x38, 0xe0
 class DisplayHelper {
 private:
     Adafruit_SSD1306 display;
+    int sda_pin = 9;
+    int scl_pin = 10;
 
 public:
-    DisplayHelper() : display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET) {}
+    DisplayHelper() {
+      // Initialize I2C
+      Wire.setPins(sda_pin, scl_pin); // Set the I2C pins before begin
+      Wire.begin();
+      display = Adafruit_SSD1306(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+    }
 
     void init() {
         if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
             Serial.println(F("SSD1306 allocation failed"));
             for (;;);
         } else {
+          display.setTextSize(1);
           Serial.println(F("Successfully initialized display"));
         }
     }
 
     void oled_draw_wifi(bool active, int last_ip_octet) {
       display.clearDisplay();
-      display.setTextSize(2);      // Set text size
-      display.setTextColor(SSD1306_WHITE); // Set text color
-      display.setCursor(0, 10);    // Set position to start printing text
-      display.println("Hello");
-      display.print("World");
 
       if (active) {
-        display.drawBitmap(98, 0, bitmap_wifi_on, 13, 13, 1);
-        display.setCursor(111, 13);
+        display.drawBitmap(113, 0, bitmap_wifi_on, 13, 13, 1);
+        display.setCursor(110, 13);
         display.setTextColor(SSD1306_WHITE);
         display.setFont(NULL);
         char ip_text[4];
-        sprintf(ip_text, "%0d3", last_ip_octet);
+        sprintf(ip_text, "%03d", last_ip_octet);
         display.println(ip_text);
       }
       else {
